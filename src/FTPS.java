e56284312d914086bec3dbd4eb6851a4cff67321
Jason Zhang <jasonzhang@Admins-MacBook-Air.local>
2014-06-12 17:16:59 -0700

    created ftps class

---
+++ src/FTPS.java
@@ -0,0 +1,30 @@
+/**
+ * Created by jasonzhang on 6/12/14.
+ */
+public class FTPS extends ClientModel {
+
+    @Override
+    public void disconnect() {
+
+    }
+
+    @Override
+    public void upload(String localFileFullName, String desiredDestinationFileName) {
+
+    }
+
+    @Override
+    public void download(String fileName) {
+
+    }
+
+    @Override
+    public boolean initilizeConnection() {
+        return false;
+    }
+
+    @Override
+    public void changeWorkingDirectory(String newDirectory) {
+
+    }
+}
